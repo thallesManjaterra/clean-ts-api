6ce5cdf8219caee48be654739f31f5f6a363a0be
thallesManjaterra <thalles.manjaterra@gmail.com>
2021-03-15 15:25:07 -0300

    feat: add express route adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express'
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+type ExpressRouteHandler = (req: Request, res: Response) => void
+
+export function adaptRoute (controller: Controller): ExpressRouteHandler {
+  return (req: Request, res: Response): void => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    controller.handle(httpRequest)
+      .then(({ statusCode, body }: HttpResponse) => {
+        res
+          .status(statusCode)
+          .json(
+            statusCode === 200
+              ? body
+              : { error: body.message }
+          )
+      })
+      .catch(console.error)
+  }
+}
