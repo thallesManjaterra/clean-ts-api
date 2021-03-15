e91ed2385cf5f34844437eca304ff6cc62a984d5
thallesManjaterra <thalles.manjaterra@gmail.com>
2021-03-15 14:06:59 -0300

    test: ensure core is enable

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('CORS Middleware', () => {
+  test('should enable CORS', async () => {
+    app.get('/test_cors')
+    await request(app)
+      .get('/test_cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+})
