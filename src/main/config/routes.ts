3c3eae40970446858c0d7c682d92acd053526e1d
thallesManjaterra <thalles.manjaterra@gmail.com>
2021-03-15 15:26:06 -0300

    feat: ensure SignUp Route return an account on success

---
+++ src/main/config/routes.ts
@@ -0,0 +1,8 @@
+import { Express, Router } from 'express'
+import { signupRoutes } from '../routes/signup-routes'
+
+export default function setupRoutes (app: Express): void {
+  const router = Router()
+  app.use('/api', router)
+  signupRoutes(router)
+}
---
+++ src/main/routes/signup-routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeSignUpController } from '../factories/signup'
+
+export function signupRoutes (router: Router): void {
+  router.post('/signup', adaptRoute(makeSignUpController()))
+}
