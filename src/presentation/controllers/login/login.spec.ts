acd306eb1b072dc30621b1d9b929632c19107db7
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-03-27 15:17:31 -0300

    test: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,25 @@
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { LoginController } from './login'
+
+describe('Login Controller', () => {
+  test('should returns 400 if no email is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
+
+interface SutTypes {
+  sut: LoginController
+}
+
+function makeSut (): SutTypes {
+  const sut = new LoginController()
+  return { sut }
+}
