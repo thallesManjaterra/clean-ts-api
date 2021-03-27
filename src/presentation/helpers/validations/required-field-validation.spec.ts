8856d41014dc06a6ca50176ccf81329a1e03769c
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-03-27 19:02:42 -0300

    test: ensure RequiredFieldValidation returns null if validation succeeds

--- src/presentation/helpers/validations/required-field-validation.spec.ts
+++ src/presentation/helpers/validations/required-field-validation.spec.ts
@@ -7,4 +7,9 @@ describe('Required Field Validation', () => {
     const error = sut.validate({})
     expect(error).toEqual(new MissingParamError('field'))
   })
+  test('should return null if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toEqual(null)
+  })
 })
