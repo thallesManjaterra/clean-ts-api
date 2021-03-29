f89b252fe627f778e2b97f8a4515ebdbc3cdc83f
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-03-29 13:04:04 -0300

    test: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -0,0 +1,36 @@
+import { AccountModel } from '../../../domain/models/account'
+import { AuthenticationDataModel } from '../../../domain/usecases/authentication'
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+import { DbAuthentication } from './db-authentication'
+
+describe('DbAuthentication Usecase', () => {
+  test('should call LoadAccountByEmailRepository with correct email', async () => {
+    class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+      async load (_email: string): Promise<AccountModel> {
+        return await Promise.resolve(makeFakeAccount())
+      }
+    }
+    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
+    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+    const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
+    const authenticationData = makeFakeAuthenticationData()
+    await sut.auth(authenticationData)
+    expect(loadSpy).toHaveBeenCalledWith(authenticationData.email)
+  })
+})
+
+function makeFakeAuthenticationData (): AuthenticationDataModel {
+  return {
+    email: 'any_email@mail.com',
+    password: 'any_passsword'
+  }
+}
+
+function makeFakeAccount (): AccountModel {
+  return {
+    id: 'any_id',
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'hashed_password'
+  }
+}
