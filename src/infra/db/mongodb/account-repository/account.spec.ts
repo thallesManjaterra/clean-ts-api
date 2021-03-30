27a9ad5a4eaf7426d8cdecf37661025eaa2e9209
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-03-30 12:54:14 -0300

    test: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,22 +1,35 @@
+import { Collection } from 'mongodb'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
 
 describe('Account Mongo Repository', () => {
+  let accountCollection: Collection
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
-  test('should return an account on success', async () => {
+  test('should return an account on add success', async () => {
     const sut = new AccountMongoRepository()
     const accountData = makeFakeAccountData()
     const account = await sut.add(accountData)
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe(accountData.name)
+    expect(account.email).toBe(accountData.email)
+    expect(account.password).toBe(accountData.password)
+  })
+  test('should return an account on loadByEmail success', async () => {
+    const sut = new AccountMongoRepository()
+    const accountData = makeFakeAccountData()
+    await accountCollection.insertOne(accountData)
+    const account = await sut.loadByEmail(accountData.email)
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe(accountData.name)
