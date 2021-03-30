daffdad031931befa94b44b63474433513b9d805
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-03-30 15:17:11 -0300

    test: ensure login route returns 401 when invalid credentials are provided

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -49,6 +49,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    test('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@mail.com',
+          password: 'any_password'
+        })
+        .expect(401)
+    })
   })
 })
 
