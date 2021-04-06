4a60b1dab9f1c3011afda000e3b3d9c15edb27f5
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-04-05 21:01:16 -0300

    test: ensure SignUpController returns 500 if Authentication throws

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -55,6 +55,14 @@ describe('Sign Up Controller', () => {
     const { email, password } = httpRequest.body
     expect(authSpy).toHaveBeenCalledWith({ email, password })
   })
+  test('should return 500 if Authentication throws', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest
+      .spyOn(authenticationStub, 'auth')
+      .mockRejectedValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
 
 interface SutTypes {
