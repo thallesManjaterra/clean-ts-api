091a5d3000d2ded59fbe3ca94c15119428049df4
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-04-09 12:06:04 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,11 +1,53 @@
 import { forbidden } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+import { AccountModel } from '../../domain/models/account'
 
 describe('Auth Middleware', () => {
   test('Should return 403 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
   })
+  test('Should call LoadAccountByToken with correct accessToken', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    const httpRequest = {
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
+  })
 })
+
+interface SutTypes {
+  sut: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+function makeSut (): SutTypes {
+  const loadAccountByTokenStub = makeLoadAccountByToken()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+  return { sut, loadAccountByTokenStub }
+}
+
+function makeLoadAccountByToken (): LoadAccountByToken {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (accessToken: string, role?: string): Promise<AccountModel> {
+      return await Promise.resolve(makeFakeAccount())
+    }
+  }
+  return new LoadAccountByTokenStub()
+}
+
+function makeFakeAccount (): AccountModel {
+  return {
+    id: 'any_id',
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'hashed_password'
+  }
+}
