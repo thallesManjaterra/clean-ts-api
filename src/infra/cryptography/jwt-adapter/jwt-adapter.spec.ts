f96c48aa53607c82bfc98f97b773b5e0dd72b55c
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-04-09 14:44:46 -0300

    test: ensure JwtAdapter calls verify with correct values

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -4,10 +4,14 @@ import { JwtAdapter } from './jwt-adapter'
 const SECRET_KEY = 'secret_key'
 const ANY_ID = 'any_id'
 const ANY_TOKEN = 'any_token'
+const ANY_VALUE = 'any_value'
 
 jest.mock('jsonwebtoken', () => ({
   sign () {
     return ANY_TOKEN
+  },
+  verify () {
+    return ANY_VALUE
   }
 }))
 
@@ -30,6 +34,14 @@ describe('Jwt Adapter', () => {
         throw new Error()
       })
       expect(sut.encrypt).toThrow()
+    })
+  })
+  describe('verify()', () => {
+    test('should call verify with correct values', () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      sut.decrypt(ANY_TOKEN)
+      expect(verifySpy).toHaveBeenCalledWith(ANY_TOKEN, SECRET_KEY)
     })
   })
 })
