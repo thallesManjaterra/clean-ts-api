50604b467d4c92bd999a15cac3aeae619253b762
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-04-09 14:11:13 -0300

    test: ensure DbloadAccountByToken returns null if LoadAccountByTokenRepository returns null

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -7,13 +7,13 @@ describe('DbloadAccountByToken Usecase', () => {
   test('should call Decrypter with correct values ', async () => {
     const { sut, decrypterStub } = makeSut()
     const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
-    await sut.load('any_token')
+    await sut.load('any_token', 'any_role')
     expect(decryptSpy).toHaveBeenCalledWith('any_token')
   })
   test('should return null if Decrypter returns null', async () => {
     const { sut, decrypterStub } = makeSut()
     jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(null)
-    const account = await sut.load('any_token')
+    const account = await sut.load('any_token', 'any_role')
     expect(account).toBe(null)
   })
   test('should call LoadAccountByTokenRepository with correct value', async () => {
@@ -21,6 +21,12 @@ describe('DbloadAccountByToken Usecase', () => {
     const loadByTokenSpy = jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken')
     await sut.load('any_token', 'any_role')
     expect(loadByTokenSpy).toHaveBeenCalledWith('any_token', 'any_role')
+  })
+  test('should return null if LoadAccountByTokenRepository returns null', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockResolvedValueOnce(null)
+    const account = await sut.load('any_token', 'any_role')
+    expect(account).toBe(null)
   })
 })
 
