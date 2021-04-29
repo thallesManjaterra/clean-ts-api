f5cb92a4d8e2d4526a76838b5c1cefcc0f614893
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-04-29 20:15:34 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct accessToken

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,12 +1,45 @@
+import { AccountModel } from '../../domain/models/account'
 import { AccessDeniedError } from '../errors'
 import { forbidden } from '../helpers/http/http-helper'
 import { AuthMiddleware } from './auth-middleware'
+import { LoadAccountByToken } from './auth-middleware-protocols'
 
 describe('Auth Middleware', () => {
-  test('should return 403 if no x-access-token exists in header', async () => {
-    const sut = new AuthMiddleware()
+  test('should return 403 if no x-access-token exists in request headers', async () => {
+    const loadAccountByTokenStub = makeLoadAccountByToken()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
     const httpRequest = {}
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
   })
+  test('should call LoadAccountByToken with correct accessToken', async () => {
+    const loadAccountByTokenStub = makeLoadAccountByToken()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    const httpRequest = {
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
+  })
 })
+
+function makeLoadAccountByToken (): LoadAccountByToken {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (accessToken: string, role?: string): Promise<AccountModel> {
+      return await Promise.resolve(makeFakeAccount())
+    }
+  }
+  return new LoadAccountByTokenStub()
+}
+
+function makeFakeAccount (): AccountModel {
+  return {
+    id: 'any_id',
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'hashed_password'
+  }
+}
