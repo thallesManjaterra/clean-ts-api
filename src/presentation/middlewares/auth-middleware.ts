cb520cd9b404f7be5c3c292f92064d29240307a0
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-04-30 15:16:19 -0300

    refactor: correct misspelling

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -4,7 +4,7 @@ import { HttpRequest, HttpResponse, Middleware, LoadAccountByToken } from './aut
 
 export class AuthMiddleware implements Middleware {
   constructor (
-    private readonly loadAccountByTokenStub: LoadAccountByToken,
+    private readonly loadAccountByToken: LoadAccountByToken,
     private readonly role?: string
   ) {}
 
@@ -12,7 +12,7 @@ export class AuthMiddleware implements Middleware {
     try {
       const accessToken = httpRequest.headers?.['x-access-token']
       if (accessToken) {
-        const account = await this.loadAccountByTokenStub.load(accessToken, this.role)
+        const account = await this.loadAccountByToken.load(accessToken, this.role)
         if (account) {
           return ok({ accountId: account.id })
         }
