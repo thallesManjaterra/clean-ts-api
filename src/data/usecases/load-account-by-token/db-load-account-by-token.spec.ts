3ef679f22ed630a58e827b58ba05e20246810434
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-04-30 15:37:43 -0300

    test: ensure DbLoadAccountByToken returns null if Decrypter returns null

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -5,8 +5,14 @@ describe('DbLoadAccountByToken Usecase', () => {
   test('should call Decrypter with correct value', async () => {
     const { sut, decrypterStub } = makeSut()
     const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
-    await sut.load('any_token')
+    await sut.load('any_token', 'admin')
     expect(decryptSpy).toHaveBeenCalledWith('any_token')
+  })
+  test('should return null if Decrypter returns null', async () => {
+    const { sut, decrypterStub } = makeSut()
+    jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(null)
+    const account = await sut.load('any_token', 'admin')
+    expect(account).toBeNull()
   })
 })
 
