1ec74924f39435c45f97ca9844b92b2f2a3e43ee
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-04-30 15:48:15 -0300

    test: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct values

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -1,4 +1,4 @@
-import { Decrypter } from './db-load-account-by-token-protocols'
+import { Decrypter, LoadAccountByTokenRepository, AccountModel } from './db-load-account-by-token-protocols'
 import { DbLoadAccountByToken } from './db-load-account-by-token'
 
 describe('DbLoadAccountByToken Usecase', () => {
@@ -14,17 +14,25 @@ describe('DbLoadAccountByToken Usecase', () => {
     const account = await sut.load('any_token', 'admin')
     expect(account).toBeNull()
   })
+  test('should call LoadAccountByTokenRepository with correct values', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    const loadByTokenSpy = jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken')
+    await sut.load('any_token', 'admin')
+    expect(loadByTokenSpy).toHaveBeenCalledWith('any_token', 'admin')
+  })
 })
 
 interface SutTypes {
   sut: DbLoadAccountByToken
   decrypterStub: Decrypter
+  loadAccountByTokenRepositoryStub: LoadAccountByTokenRepository
 }
 
 function makeSut (): SutTypes {
   const decrypterStub = makeDecrypter()
-  const sut = new DbLoadAccountByToken(decrypterStub)
-  return { sut, decrypterStub }
+  const loadAccountByTokenRepositoryStub = makeLoadAccountByTokenRepository()
+  const sut = new DbLoadAccountByToken(decrypterStub, loadAccountByTokenRepositoryStub)
+  return { sut, decrypterStub, loadAccountByTokenRepositoryStub }
 }
 
 function makeDecrypter (): Decrypter {
@@ -34,4 +42,22 @@ function makeDecrypter (): Decrypter {
     }
   }
   return new DecrypterStub()
+}
+
+function makeLoadAccountByTokenRepository (): LoadAccountByTokenRepository {
+  class LoadAccountByTokenRepositoryStub implements LoadAccountByTokenRepository {
+    async loadByToken (token: string, role?: string): Promise<AccountModel> {
+      return await Promise.resolve(makeFakeAccount())
+    }
+  }
+  return new LoadAccountByTokenRepositoryStub()
+}
+
+function makeFakeAccount (): AccountModel {
+  return {
+    id: 'any_id',
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'hashed_password'
+  }
 }
