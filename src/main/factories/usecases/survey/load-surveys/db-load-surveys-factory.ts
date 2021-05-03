1b8185d6640e16de693778318e07e3ebc99c0fc9
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-05-03 20:03:53 -0300

    feat: ensure load surveys route returns 403 if no access token is provided

---
+++ src/main/factories/usecases/survey/load-surveys/db-load-surveys-factory.ts
@@ -0,0 +1,9 @@
+import { DbLoadSurveys } from '../../../../../data/usecases/survey/load-surveys/db-load-surveys'
+import { LoadSurveys } from '../../../../../domain/usecases/load-surveys'
+import { SurveyMongoRepository } from '../../../../../infra/db/mongodb/survey/survey-mongo-repository'
+
+export function makeDbLoadSurveys (): LoadSurveys {
+  const surveyMongoRepository = new SurveyMongoRepository()
+  const dbLoadSurveys = new DbLoadSurveys(surveyMongoRepository)
+  return dbLoadSurveys
+}
