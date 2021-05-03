42d3249eed4c62830dad799239e5cf005eb4d7ff
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-05-03 14:09:15 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/survey/load-surveys/db-load-surveys.ts
@@ -0,0 +1,12 @@
+import { LoadSurveysRepository, SurveyModel, LoadSurveys } from './db-load-surveys-protocols'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (
+    private readonly loadSurveysRepository: LoadSurveysRepository
+  ) {}
+
+  async load (): Promise<SurveyModel[]> {
+    await this.loadSurveysRepository.loadAll()
+    return null
+  }
+}
