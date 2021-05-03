a1d8874a4433593ab55d7ce8c3983fbd1c01ca49
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-05-03 13:19:21 -0300

    test: ensure LoadSurveysController calls LoadSurveys usecase

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -0,0 +1,45 @@
+import { LoadSurveysController } from './load-surveys-controller'
+import { LoadSurveys, SurveyModel } from './load-surveys-controller-protocols'
+import mockDate from 'mockdate'
+
+describe('LoadSurveys Controller', () => {
+  beforeAll(() => {
+    mockDate.set(new Date())
+  })
+  afterAll(() => {
+    mockDate.reset()
+  })
+  test('should call LoadSurveys', async () => {
+    class LoadSurveysStub implements LoadSurveys {
+      async load (): Promise<SurveyModel[]> {
+        return await Promise.resolve(makeFakeSurveys())
+      }
+    }
+    const loadSurveysStub = new LoadSurveysStub()
+    const sut = new LoadSurveysController(loadSurveysStub)
+    const loadSpy = jest.spyOn(loadSurveysStub, 'load')
+    await sut.handle({})
+    expect(loadSpy).toHaveBeenCalled()
+  })
+})
+
+function makeFakeSurveys (): SurveyModel[] {
+  return [{
+    id: 'any_id',
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    }],
+    date: new Date()
+  }, {
+    id: 'another_id',
+    question: 'another_question',
+    answers: [{
+      image: 'another_image',
+      answer: 'another_answer'
+    }],
+    date: new Date()
+
+  }]
+}
