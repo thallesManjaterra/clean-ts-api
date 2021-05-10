1844de29d080b9a11a239cf59a62c39c723f9302
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-05-10 10:53:39 -0300

    test: ensure DbSaveSurveyResult throws if SaveSurveyResultRepository throws

--- src/data/usecases/survey-result/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/survey-result/save-survey-result/db-save-survey-result.spec.ts
@@ -3,14 +3,29 @@ import { DbSaveSurveyResult } from './db-save-survey-result'
 
 describe('DbSaveSurveyResult Usecase', () => {
   test('should call SaveSurveyResultRepository with correct values', async () => {
-    const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepository()
-    const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
+    const { sut, saveSurveyResultRepositoryStub } = makeSut()
     const saveSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save')
     const surveyResultData = makeFakeSurveyResultData()
     await sut.save(surveyResultData)
     expect(saveSpy).toHaveBeenCalledWith(surveyResultData)
   })
+  test('should throw if SaveSurveyResultRepository throws', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut()
+    jest.spyOn(saveSurveyResultRepositoryStub, 'save').mockRejectedValueOnce(new Error())
+    await expect(sut.save).rejects.toThrow()
+  })
 })
+
+interface SutTypes {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositoryStub: SaveSurveyResultRepository
+}
+
+function makeSut (): SutTypes {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepository()
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
+  return { sut, saveSurveyResultRepositoryStub }
+}
 
 function makeSaveSurveyResultRepository (): SaveSurveyResultRepository {
   class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
