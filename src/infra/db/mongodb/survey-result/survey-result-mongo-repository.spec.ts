41acbf6a869b27652839f52f6d7597c196db159b
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-05-10 13:36:02 -0300

    test: ensure SurveyResultMongoRepository inserts a new survey result if its new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -0,0 +1,81 @@
+import { AddSurveyModel } from '@/domain/usecases/survey/add-survey'
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyResultMongoRepository } from './survey-result-mongo-repository'
+import mockDate from 'mockdate'
+import { AddAccountModel } from '@/domain/usecases/account/add-account'
+
+let surveyResultCollection: Collection,
+  surveyCollection: Collection,
+  accountCollection: Collection
+describe('Survey Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+    mockDate.set(new Date())
+  })
+  beforeEach(async () => {
+    surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    await surveyResultCollection.deleteMany({})
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    mockDate.reset()
+  })
+  describe('save()', () => {
+    test('should add a new survey result if its new', async () => {
+      const sut = new SurveyResultMongoRepository()
+      const { id: accountId } = await insert(
+        accountCollection,
+        makeFakeAccountData()
+      )
+      const {
+        id: surveyId,
+        answers: [{ answer: firstAnswer }]
+      } = await insert(surveyCollection, makeFakeSurveyData())
+      const surveyResult = await sut.save({
+        accountId,
+        surveyId,
+        answer: firstAnswer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toBeTruthy()
+      expect(surveyResult.answer).toBe(firstAnswer)
+    })
+  })
+})
+
+async function insert (collection: Collection, data: any): Promise<any> {
+  const {
+    ops: [document]
+  } = await collection.insertOne(data)
+  return MongoHelper.formatId(document)
+}
+
+function makeFakeAccountData (): AddAccountModel {
+  return {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'hashed_password'
+  }
+}
+
+function makeFakeSurveyData (): AddSurveyModel {
+  return {
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer'
+      },
+      {
+        answer: 'another_answer'
+      }
+    ],
+    date: new Date()
+  }
+}
