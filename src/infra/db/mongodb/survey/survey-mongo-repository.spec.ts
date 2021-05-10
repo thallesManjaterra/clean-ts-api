c3c299c6362a2e41ff8fe29ebe2f2c707104358c
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-05-10 11:39:46 -0300

    test: ensure SurveyMongoRepository loads a survey by id

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -2,11 +2,13 @@ import { AddSurveyModel } from '@/domain/usecases/add-survey'
 import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { SurveyMongoRepository } from './survey-mongo-repository'
+import mockDate from 'mockdate'
 
 let surveyCollection: Collection
 describe('Survey Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
+    mockDate.set(new Date())
   })
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
@@ -14,6 +16,7 @@ describe('Survey Mongo Repository', () => {
   })
   afterAll(async () => {
     await MongoHelper.disconnect()
+    mockDate.reset()
   })
   describe('add()', () => {
     test('should add a new survey', async () => {
@@ -40,6 +43,16 @@ describe('Survey Mongo Repository', () => {
       const sut = new SurveyMongoRepository()
       const surveys = await sut.loadAll()
       expect(surveys.length).toBe(0)
+    })
+  })
+  describe('loadById()', () => {
+    test('should load survey by id', async () => {
+      const sut = new SurveyMongoRepository()
+      const { ops: [{ _id }] } = await surveyCollection.insertOne(makeFakeSurveyData())
+      const survey = await sut.loadById(_id)
+      expect(survey).toBeTruthy()
+      expect(survey.id).toBeTruthy()
+      expect(survey).toMatchObject(makeFakeSurveyData())
     })
   })
 })
